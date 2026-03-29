c8e762cc8f10e046bf34ac0ac205ce0dd6177688
candy-ii <thelastcandyii@gmail.com>
2026-03-29 08:41:11 +0600

    Changed port for hosting.

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'Server is running' });
 });
 
-app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001');
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server running on port ${process.env.PORT || 3001}`);
 });
